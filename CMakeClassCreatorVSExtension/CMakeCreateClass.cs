0c8a1f69802f82c84c40e36c8374c3756fc73c8c
Frank Goyens <frankgoyens@hotmail.com>
2020-12-31 21:46:10 +0100

    Initial commit
    There is a context menu item "CMake Create Class" for the current file tab, clicking this will show some placeholder and also the file (which is handy to later use as a reference file)

---
+++ CMakeClassCreatorVSExtension/CMakeCreateClass.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+using EnvDTE;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Task = System.Threading.Tasks.Task;
+
+namespace CMakeClassCreatorVSExtension
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class CMakeCreateClass
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("1c64a012-295d-4049-8248-f985c7d42078");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CMakeCreateClass"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private CMakeCreateClass(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static CMakeCreateClass Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in CMakeCreateClass's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            Instance = new CMakeCreateClass(package, commandService);
+        }
+
+        private async Task<EnvDTE80.DTE2> GetApplicationObjectAsync()
+        {
+            return (await ServiceProvider.GetServiceAsync(typeof(DTE))) as EnvDTE80.DTE2;
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private async void Execute(object sender, EventArgs e)
+        {
+            var applicationObject = await GetApplicationObjectAsync();
+
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+            var documentName = applicationObject.ActiveDocument.FullName;
+
+            string message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback(), called on document: {1}", this.GetType().FullName, documentName);
+            string title = "CMakeCreateClass";
+
+            // Show a message box to prove we were here
+            VsShellUtilities.ShowMessageBox(
+                this.package,
+                message,
+                title,
+                OLEMSGICON.OLEMSGICON_INFO,
+                OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# CMakeClassCreatorVSExtension
+This is a Visual Studio Plugin for CMake Class Creator. This makes it possible to create cmake classes from within the Visual Studio editor.
+
+(That is what it will do eventually at least, this project is currently is a proof of concept phase)
